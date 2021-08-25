73a1aa214a87e7d0150f335ebe340e4cd846b20b
Zoltan199 <valerasergio19@gmail.com>
2021-08-24 21:38:55 -0500

    Se crearon las funciones de los botones correspondientes en el documento figuras.html

--- figuras.js
+++ figuras.js
@@ -45,3 +45,52 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value; //Sacar el valor del input ingresado por el usuario
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value; //Sacar el valor del input ingresado por el usuario
+    const area = areaCuadrado(value);
+    alert(area);
+
+}
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("input1Triangulo");
+    value1 = input1.value;
+    const input2 = document.getElementById("input2Triangulo");
+    value2 = input2.value;
+    const input3 = document.getElementById("input3Triangulo");
+    value3 = input3.value;
+    const perimetro = perimetroTriangulo(value1, value2, value3);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo(){
+    const input1 = document.getElementById("input3Triangulo");
+    value1 = input1.value;
+    const input2 = document.getElementById("input4Triangulo");
+    value2 = input2.value;
+    const area = areaTriangulo(value1, value2);
+    alert(area);
+}
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("inputCirculo");
+    value = input.value;
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo(){
+    const input = document.getElementById("inputCirculo");
+    value = input.value;
+    const area = areaCirculo(value);
+    alert(area);
+}
