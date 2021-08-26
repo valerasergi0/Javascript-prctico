700f568c9b3a13fe6ab98dadfa2dc0b827d746be
Zoltan199 <valerasergio19@gmail.com>
2021-08-26 14:55:20 -0500

    Creación de la primera formula para obtener el precio con descuento

--- Porcentajes/porcentajes.js
+++ Porcentajes/porcentajes.js
@@ -1,1 +1,19 @@
 //Porcentajes y descuentos
+
+const precioOriginal = 100;
+const descuento = 15;
+
+
+//console.log({
+  //  precioOriginal,
+    //descuento,
+    //porcentajePrecioConDescuento,
+    //precioConDescuento
+//});
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = ((precio * porcentajePrecioConDescuento) / 100);
+    return precioConDescuento;
+
+}
