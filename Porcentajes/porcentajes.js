e22fb8f24c24020bb81212aa46edbd4215a2ee18
Zoltan199 <valerasergio19@gmail.com>
2021-08-27 12:43:52 -0500

    Creación del la función para retornar el valor con descuento e imprimirlo a través de un párrafo

--- Porcentajes/porcentajes.js
+++ Porcentajes/porcentajes.js
@@ -17,3 +17,14 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount(){
+  const inputPrice = document.getElementById("inputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  const resultP =  document.getElementById("resultP");
+  resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
