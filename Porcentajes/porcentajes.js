faf4afb6a096bfc1115664d4fa460e5e6432ee6e
Zoltan199 <valerasergio19@gmail.com>
2021-08-26 19:33:59 -0500

    Agregué un nuevo formulario al archivo porcentajes.html

--- Porcentajes/porcentajes.js
+++ Porcentajes/porcentajes.js
@@ -15,5 +15,5 @@ function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = ((precio * porcentajePrecioConDescuento) / 100);
     return precioConDescuento;
-
 }
+
