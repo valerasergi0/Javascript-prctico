70932d26599c9e413725e49e8811e05ecf6d754a
Zoltan199 <valerasergio19@gmail.com>
2021-08-28 18:05:26 -0500

    Mejoramiento de la función para calcular la media artimética

--- Promedios/promedio.js
+++ Promedios/promedio.js
@@ -11,10 +11,15 @@ const lista1 = [
 
 
 function calcularMediaArtimetica(lista){
-    let sumaLista = 0;
-    for(let i = 0; i < lista.length;  i++){
-        sumaLista = sumaLista + lista[i];
-    }
+    //let sumaLista = 0;
+    //for(let i = 0; i < lista.length;  i++){
+    //    sumaLista = sumaLista + lista[i];
+    //}
+    const sumaLista = lista.reduce( //Este método recibe una función, funciona para hacer un ciclo
+        function (valorAcumulado = 0, nuevoElemento){ //Rquiere dos parámetros
+            return valorAcumulado + nuevoElemento;
+        }
+    )
     const promedioLista = sumaLista / lista.length;
     return promedioLista;
 };
