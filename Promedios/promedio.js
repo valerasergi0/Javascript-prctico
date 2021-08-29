2208565878e26f66988ea432bd0f07292bed97d9
Zoltan199 <valerasergio19@gmail.com>
2021-08-28 19:54:20 -0500

    Creación de los métodos para detenerminar la moda

--- Promedios/promedio.js
+++ Promedios/promedio.js
@@ -40,5 +40,42 @@ function cacularMediana(lista){
     return mediana;
 };
 
+function calcularModa(){
+
+};
+
+const lista1 = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1,
+  ];
+
+const lista1Count = {};
+
+lista1.map(
+    function (elemento){
+        if(lista1Count[elemento]){
+            lista1Count[elemento] += 1;
+        }else{
+            lista1Count[elemento] = 1;
+        }
+    }
+);
+
+const lista1Array = Object.entries(lista1Count).sort( //Recibe como argumento el objeto que se convertirá en un array
+    function(a, b){
+        return a[1] -b[1];
+    }   
+    
+);
+
 
 
